d4ea7b5e58ae8b160ef8ea763843bd6b5bfbe4eb
Ross Cunning <ross.cunning@gmail.com>
2016-04-02 10:36:21 -1000

    plot reef 42

--- analysis.R
+++ analysis.R
@@ -197,7 +197,7 @@ for (i in 1:nlevels(Mcap2015.ff$cluster)) {
 }
 
 
-shufflers <- c(11,31,40,54,71,78,80,119,125,130,215,223,227)
+shufflers <- c(11,31,40,54,71,119,125,223,227)
 nonshufflers <- as.numeric(as.character(levels(Mcap2015.ff$colony)[!levels(Mcap2015.ff$colony) %in% shufflers]))
 par(mfrow=c(1,2))
 for (g in c("shufflers", "nonshufflers")) {
@@ -211,11 +211,17 @@ for (g in c("shufflers", "nonshufflers")) {
   }
 }
 
-
-
-
-
-
+Mcap.ff.all$shuff <- ifelse(Mcap.ff.all$colony %in% shufflers, 1, 0)
+anova(lm(shuff ~ vis, data=Mcap.ff))
+plot(factor(shuff) ~ interaction(factor(vis), factor(reef)), data=Mcap.ff)
+minsh <- aggregate(data.frame(min=log(Mcap.ff$tot.SH)), by=list(colony=Mcap.ff$colony), FUN=min)
+Cs <- unique(Mcap2014.f[Mcap2014.f$tdom=="C", "colony"])
+minsh$shuff <- ifelse(minsh$colony %in% shufflers, 1, 0)
+minsh <- minsh[minsh$colony %in% Cs, ]
+plot(shuff ~ min, data=minsh)
+lr <- glm(shuff~min,data=minsh,family=binomial)
+lr
+predict(lr,data.frame(x=-6),type="response")
 
 
 # • Analysis: Symbiodinium community structure --------------------------
@@ -539,26 +545,27 @@ text(xpd=T, y=quantile(par("usr")[3:4], 0) * -1.05 - 2.5, pos=1, cex=0.9,
 #   From January to May, fit a linear model (2nd element of degree=1)
 #   Function is continuous at time=82 days (smooth=0)
 Mcap.ff$days <- as.numeric(Mcap.ff$date - as.Date("2015-08-11"))
-offset <- 0  # optional to center "days" axis at any point
+#offset <- 0  # optional to center "days" axis at any point
 sp <- function(x) gsp(x, knots=c(51,85,128), degree=c(2,3,2,2))
 #sp <- function(x) cs(x)
 #sp <- function(x) bs(x, knots=c(71,128))
 # Build full model with fixed effects of vis, tdom, reef, and time, random effect of colony
 Mcapdf <- Mcap.ff[Mcap.ff$reef!="42",]
-mod.all.full <- lmerTest::lmer(log(Mcapdf$tot.SH) ~ sp(Mcapdf$days) * Mcapdf$vis * Mcapdf$reef + (1 | Mcapdf$colony))
+#mod.all.full <- lmerTest::lmer(log(Mcapdf$tot.SH) ~ sp(Mcapdf$days) * Mcapdf$vis * Mcapdf$reef + (sp(Mcapdf$days) | Mcapdf$colony))
 #mod.all.full <- lmerTest::lmer(log(tot.SH) ~ poly(days, 3) * vis * reef + (1 | colony), data=Mcap.ff[Mcap.ff$reef!="42",])
 #plot(Effect(c("days", "vis", "reef"), mod.all.full, xlevels=list(days=unique(Mcap.ff$days))), 
 #     multiline=T, z.var="reef", ci.style="bars")
 # Test significance of fixed effects by backwards selection
 #modselect <- step(mod.all.full, lsmeans.calc=F, difflsmeans.calc=F, alpha.fixed=0.05)
+#modselect
 #summary(mod.all.full)
 # Rebuild model omitting non-significant fixed effects
-mod.all <- mod.all.full
+#mod.all <- mod.all.full
 # Identify outliers with standardized residuals > 2.5
-out <- abs(residuals(mod.all)) > sd(residuals(mod.all)) * 2.5
-Mcap.ff[out, ]  # outlying data points
+#out <- abs(residuals(mod.all)) > sd(residuals(mod.all)) * 2.5
+#Mcap.ff[out, ]  # outlying data points
 # Refit model without outliers
-Mcapdf <- Mcapdf[!out, ]
+#Mcapdf <- Mcapdf[!out, ]
 mod.all <- lmerTest::lmer(log(tot.SH) ~ sp(days) * vis * reef + (1 | colony), data=Mcapdf)
 #mod.all <- mod.all.full
 # Print and save ANOVA table for model
@@ -567,10 +574,9 @@ anovatab <- anova(mod.all)
 # pseudo-r2 value-- squared correlation between fitted and observed values
 summary(lm(model.response(model.frame(mod.all)) ~ fitted(mod.all)))$r.squared
 
-
 # Plotting function
 plotreefs <- function(mod, n) {
-  dat <- get(as.character(summary(mod.all)$call$data))
+  dat <- get(as.character(summary(mod)$call$data))
   dat <- droplevels(dat)
   levs <- expand.grid(reef=levels(dat$reef), vis=levels(dat$vis), days=as.numeric(levels(as.factor(dat$days))))
   datlevs <- list(interaction(dat$reef, dat$vis, dat$days))
@@ -591,11 +597,11 @@ plotreefs <- function(mod, n) {
   predlist <- lapply(predlist, function(x) rev(split(x, f=x$vis)))
   par(mgp=c(1.75,0.4,0), oma=c(0,0,0,0))
   par(mar=c(0,3,0.3,1))
-  layout(mat=matrix(seq_len(nlevels(dat$reef))))
+  layout(mat=matrix(seq_len(nlevels(dat$reef)+1)))
   for (reef in levels(dat$reef)) {
     with(datlist[[reef]], {
       # Create plot frame for each reef
-      plot(NA, xlim=range(dat$days), ylim=c(-11,-1), xaxt="n", bty="n", tck=-0.03, ylab="ln S/H")
+      plot(NA, xlim=range(dat$days), ylim=c(-9,-1), xaxt="n", bty="n", tck=-0.03, ylab="ln S/H")
       title(paste("Reef", reef), line=-0.9, adj=0, outer=F)
       # Plot model fit line and shaded CI for bleached and/or not bleached corals
       with(predlist[[reef]], {
@@ -612,9 +618,9 @@ plotreefs <- function(mod, n) {
     })
     #rect(xleft = 0, ybottom = -6, xright = 82, ytop = -1, lty = 3, border="black")
   }
-  axis(side=1, at=as.numeric(as.Date(c("2014-11-01", "2014-12-01", "2015-01-01", "2015-02-01", 
-                                       "2015-03-01", "2015-04-01", "2015-05-01")) - as.Date("2014-10-24")),
-       labels=c("Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May"))
+  axis(side=1, at=as.numeric(as.Date(c("2015-08-01", "2015-09-01", "2015-10-01", "2015-11-01", 
+                                       "2015-12-01", "2016-01-01", "2016-02-01")) - as.Date("2015-08-11")),
+       labels=c("Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb"))
   return(list(predlist=predlist, datlist=datlist))
 }
 # Plot
@@ -622,4 +628,45 @@ reefcols <- list(`25`="#bebada", `44`="#8dd3c7", HIMB="#d9d9d9", `42`="green")
 vislty <- list("bleached"=2, "not bleached"=1)
 vispch <- list("bleached"=24, "not bleached"=21)
 visbg <- list("bleached"="white", "not bleached"="black")
-modelplot <- plotreefs(mod.all, 999)
+modelplot <- plotreefs(mod.all, 99)
+
+Mcap42 <- Mcap.ff[Mcap.ff$reef==42,]
+sp2 <- function(x) gsp(x, knots=c(85,128), degree=c(2,2,2))
+mod.42 <- lmerTest::lmer(log(tot.SH) ~ sp2(days) * vis + (1 | colony), data=Mcap42)
+plot42 <- function(mod, n) {
+  dat <- get(as.character(summary(mod)$call$data))
+  dat <- droplevels(dat)
+  levs <- expand.grid(vis=levels(dat$vis), days=as.numeric(levels(as.factor(dat$days))))
+  datlevs <- list(interaction(dat$vis, dat$days))
+  datsumm <- data.frame(levs,
+                        mean=with(dat, aggregate(log(tot.SH), by=datlevs, FUN=mean)$x),
+                        sd=with(dat, aggregate(log(tot.SH), by=datlevs, FUN=sd)$x),
+                        se=with(dat, aggregate(log(tot.SH), by=datlevs, FUN=function(x) sd(x)/sqrt(length(x)))$x),
+                        conf95=with(dat, aggregate(log(tot.SH), by=datlevs, FUN=function(x) sd(x)/sqrt(length(x)) * qt(0.975, length(x)-1))$x))
+  datlist <- split(datsumm, f=datsumm$vis)
+  pred <- expand.grid(days=seq(min(dat$days), max(dat$days)), vis=levels(dat$vis))
+  bootfit <- bootMer(mod, FUN=function(x) predict(x, pred, re.form=NA), nsim=n)
+  # Extract 90% confidence interval on predicted values
+  pred$fit <- predict(mod, pred, re.form=NA)
+  pred$lci <- apply(bootfit$t, 2, quantile, 0.05)
+  pred$uci <- apply(bootfit$t, 2, quantile, 0.95)
+  predlist <- split(pred, f=pred$vis)
+ 
+      plot(NA, xlim=c(0, max(dat$days)), ylim=c(-9,-1), xaxt="n", bty="n", tck=-0.03, ylab="ln S/H")
+      title("Reef 42", line=-0.9, adj=0, outer=F)
+      # Plot model fit line and shaded CI for bleached and/or not bleached corals
+      with(predlist, {
+        lapply(predlist, function(vis) {
+          addpoly(vis$days, vis$lci, vis$uci, col=alpha("green", 0.4), xpd=NA)
+          lines(vis$days, vis$fit, lty=vislty[[vis$vis[1]]])
+        })
+      })
+      # Plot raw data +/- standard error
+      lapply(datlist, function(vis) {
+        arrows(vis$days, vis$mean + vis$se, vis$days, vis$mean - vis$se, code=3, angle=90, length=0.03, xpd=NA)
+        points(vis$days, vis$mean, pch=vispch[[vis$vis[1]]], bg=visbg[[vis$vis[1]]])
+      })
+    #rect(xleft = 0, ybottom = -6, xright = 82, ytop = -1, lty = 3, border="black")
+  return(list(predlist=predlist, datlist=datlist))
+}
+plot42(mod.42, 99)
